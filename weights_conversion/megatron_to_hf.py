aec596ea46c66cad6068f7f21e938792a192b803
Ubuntu <pretraining@nvidia-benchmark1.w2fl5ig43duujoa2us5huzkkof.bx.internal.cloudapp.net>
2025-02-17 08:38:34 +0000

    torch to hf

--- weights_conversion/megatron_to_hf.py
+++ weights_conversion/megatron_to_hf.py
@@ -99,12 +99,14 @@ def write_llama_model(model_path,
     args = loaded['args']
 
     # loaded = loaded['model']['language_model']
-    loaded = loaded['model']
+    # loaded = loaded['model']
 
-    # print('asas')
-    # print(loaded['model'])
-    # print('asaas')
-    # print(loaded['model'].keys())
+    print('as')
+    print(loaded.keys())
+    print('asas')
+    print(loaded['model'])
+    
+    print(loaded['model'].keys())
     if 'transformer' not in loaded:  # normalize key names
         loaded["transformer"] = loaded.pop("encoder")
         for key in list(loaded["transformer"].keys()):
